c8b5d706e66a3f5489e355939ea59e44323eee9b
oquevedo <oquevedo@me.com>
2014-03-25 20:59:10 -0300

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Libro::Application.config.secret_key_base = '25a7afc6a33c4bc038d83ec4e648a3da0785d6a91ba16735e7943cc0c34c49426246e41c5352a9f7a051078d14e27e55c50dae9864a5b11765befe1c94db6282'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Libro::Application.config.session_store :cookie_store, key: '_libro_session'
